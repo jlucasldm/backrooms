409d61fb0650c3e790435d991918b9af604f66a6
Lucas <jlucas.ldm@gmail.com>
2023-06-02 18:19:15 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,89 @@
+# Visitando uma Maquete Virtual - The Backrooms
+
+## Contexto
+
+Este projeto foi desenvolvido para a disciplina de Computação Gráfica do curso de Ciência da Computação da Universidade Federal da Bahia (UFBA). O objetivo do projeto é construir uma aplicação em [Three.JS](https://threejs.org/) que permita simular um passeio por uma **Maquete Virtual**. A maquete utilizada foi um modelo de um conto popular de terror da internet, The Backrooms. 
+
+The Backrooms seriam corredores largos e vazios  cuja arquitetura, iluminação e demais características causariam estranheza. As histórias em torno dos Backrooms mudam de narrador a narrador. Este trabalho se baseia no conto ambientado em torno de uma criatura e uma exploração científica. Os modelos escolhidos para a maquete e personagens, assim como controle de iluminação, levaram em consideração esse contexto. 
+
+De acordo com a especificação do projeto, foram fornecidas ao usuário 3 modalidades de percurso:
+
+1. **Modo Visita Interna**: O usuário poderá andar pela maquete, visitando seu interior, como uma pessoa o faria em um modelo real;
+
+2. **Modo Visita Externa**: Nesse modo o usuário pode "voar como um drone" no lado externo da maquete virtual. É possível selecionar distintos planos de corte para visualizar o interior da maquete;
+
+3. **Modo Visita Guiada**: 3 pontos de interesse foram definidos na maquete, onde o usuário é "transportado" para o local podendo apenas observar o ambiente ao seu redor, sem possibilidade de deslocamento.
+
+## Modelos
+
+Neste projeto foram utilizados 3 modelos 3D, sendo eles:
+
+1. **Backrooms**: Modelo da maquete virtual, contendo os corredores e salas que compõem o cenário. O modelo foi encontrado no site SketchFab[^1] e importado como .gltf.
+
+2. **Cientista**: Modelo do personagem que o usuário encontra durante a visita guiada. O modelo foi encontrado no site SketchFab[^2] e importado como .gltf. Duas instâncias desse modelo foram importadas, dando ao usuário a noção de escala do cenário.
+
+3. **Coisa**: Modelo da criatura que o usuário encontra durante a visita interna. O modelo foi encontrado no site SketchFab[^3] e importado como .gltf. Uma instância desse modelo foi importada, propositalmente maior que os demais modelos para garantir a impressão de estranheza e imponência da criatura.
+
+## Câmeras e Controles de Movimento
+
+Foram utilizadas 5 câmeras, sendo elas:
+
+1. **Câmera Interna (1)**: Câmera que o usuário controla durante a visita interna. A câmera é posicionada na altura do personagem, e o usuário pode movimentá-la utilizando o mouse e a barra de espaço. 
+
+* Barra de espaço: libera a movimentação do personagem e da câmera. Pressione novamente para travar a movimentação. Caso o comportamento não seja o esperado, clique na cena para garantir que o foco está na aplicação.
+* Movimento do mouse: movimenta a câmera.
+* Botão esquerdo do mouse: movimenta o personagem para frente.
+* Botão direito do mouse: movimenta o personagem para trás.
+
+Foi utilizado o controle de movimento FirstPersonControls, disponível no Three.JS, para controlar a câmera interna. O controle foi customizado para que a movimentação seja mais lenta e suave. O personagem pode atravessar paredes, mas está limitado ao espaço da maquete.
+
+2. **Câmera Externa (1)**: Câmera que o usuário controla durante a visita externa. A câmera é posicionada acima da maquete, e o usuário pode movimentá-la utilizando o mouse. A câmera orbita naturalmente em torno da maquete, e o usuário pode controlar a distância da câmera utilizando a roda do mouse.
+
+* Botão esquerdo do mouse: movimenta a orientação da maquete.
+* Botão direito do mouse: movimenta a posição da câmera no espaço.
+* Roda do mouse: controla a distância da câmera em relação à maquete.
+
+Foi utilizado o controle de movimento OrbitControls, disponível no Three.JS, para controlar a câmera externa. O controle foi customizado para que a movimentação seja mais lenta e suave. A câmera pode atravessar paredes.
+
+3. **Câmera Guiada (3)**: Câmeras escolhidas em três pontos fixos da maquete onde o usuário pode ser transportado durante a visita guiada. Não há possibilidade de movimento de câmera ou de movimentação.
+
+Foi utilizado o controle FlyControls, disponível no Three.JS, para controlar a câmera guiada.
+
+## Iluminação
+Duas abordagens diferentes foram adotadas para a iluminação da maquete, de acordo com o modo de visualização escolhido pelo usuário.
+
+1. **Visita Interna e Visita Guiada**: Pontos de luz distintos foram posicionados na maquete simulando os pontos de luz dos corredores. Foi utilizado PointLight, disponível no Three.JS, para criar os pontos de luz. A intensidade da luz foi reduzida para que o usuário tenha a sensação de que está em um ambiente escuro e intimidador.
+
+2. **Visita Externa**: Foi utilizado DirectionalLight, disponível no Three.JS, para criar uma luz direcional que ilumina a maquete como um todo. O ponto de luz foi colocado acima da maquete, simulando a luz do sol. Além disso, foi utilizado o AmbientLight, disponível no Three.JS, para criar uma luz ambiente que ilumina a maquete como um todo. O uso de ambas as luzes garantes uma visibilidade mais apropriada do modelo no modo de visita externa.
+
+## Planos de Corte
+Foram utilizados 3 planos de corte, sendo eles:
+
+1. **Plano de Corte YZ**: Plano de corte que permite visualizar o interior da maquete na direção do eixo X. O plano de corte é posicionado no centro da maquete, e o usuário pode movimentá-lo utilizando os controladores do GUI.
+
+2. **Plano de Corte XZ**: Plano de corte que permite visualizar o interior da maquete na direção do eixo Y. O plano de corte é posicionado na base da maquete, e o usuário pode movimentá-lo utilizando os controladores do GUI.
+
+3. **Plano de Corte XY**: Plano de corte que permite visualizar o interior da maquete na direção do eixo Z. O plano de corte é posicionado na base da maquete, e o usuário pode movimentá-lo utilizando os controladores do GUI.
+
+## Controles GUI
+Foram utilizados 5 controladores GUI, sendo eles:
+
+1. **Controlador de Modo (tipoVisita)**: Controlador que permite ao usuário escolher o modo de visualização da maquete. O usuário pode escolher entre os modos de visita interna, visita externa e visita guiada.
+
+2. **Controlador de Câmera da Visita Guiada (visitaGuiadaCamera)**: Controlador que permite ao usuário escolher a câmera que será utilizada na visita guiada. O usuário pode escolher entre as câmeras guiadas 1, 2 e 3.
+
+3. **Controlador de Plano de Corte (cortePlano)**: Controlador que permite ao usuário escolher o plano de corte que será utilizado na visita externa. O usuário pode escolher entre os planos de corte YZ, XZ e XY.
+
+4. **Controlador de Posição do Plano de Corte**: Controlador que permite ao usuário escolher a posição do plano de corte que será utilizado na visita externa. O usuário pode escolher a posição do plano de corte utilizando valores entre -17.0 e 17.0.
+
+5. **Controlador de Rotação Automática da Visita Externa**: Controlador que permite ao usuário escolher se a câmera externa irá rotacionar automaticamente em torno da maquete. O usuário pode escolher entre os valores true e false.
+
+## Execução
+Para executar o projeto, basta abrir o arquivo `index.html` em um navegador web utilizando um servidor local. O projeto foi testado nos navegadores Mozilla Firefox e Microsoft Edge.
+
+## Referências
+[1]"BackRooms" (https://skfb.ly/otQY7) by Huuxloc is licensed under Creative Commons Attribution (http://creativecommons.org/licenses/by/4.0/).
+
+[2]"Hazmat (SCP Containment Breach)" (https://skfb.ly/ooV68) by SCP Games is licensed under Creative Commons Attribution (http://creativecommons.org/licenses/by/4.0/).
+
+[3]"Bacteria Backrooms" (https://skfb.ly/o9xsN) by PipotGZ is licensed under Creative Commons Attribution (http://creativecommons.org/licenses/by/4.0/).
---
+++ main/script.js
@@ -0,0 +1,365 @@
+/*
+TODO:
+- [ ] Animar modelos
+*/
+// Importando Three.js
+import * as THREE from 'three';
+
+// Importando módulo para controle de entrada de parâmetros (GUI)
+import { GUI } from '../../Assets/scripts/three.js/examples/jsm/libs/lil-gui.module.min.js'
+
+// Importando módulos para controle de câmera
+import { OrbitControls } from '../../Assets/scripts/three.js/examples/jsm/controls/OrbitControls.js';
+import { FirstPersonControls } from '../../Assets/scripts/three.js/examples/jsm/controls/FirstPersonControls.js';
+import { FlyControls } from '../../Assets/scripts/three.js/examples/jsm/controls/FlyControls.js';
+
+// Importando módulos para carregamento de modelos
+import { GLTFLoader } from '../Assets/scripts/three.js/examples/jsm/loaders/GLTFLoader.js';
+import { FBXLoader } from '../Assets/scripts/three.js/examples/jsm/loaders/FBXLoader.js';
+import * as SkeletonUtils from '../Assets/scripts/three.js/examples/jsm/utils/SkeletonUtils.js';
+
+// Configurando as variáveis globais
+var scene,
+	renderer,
+	cameraExterna,
+	cameraInterna,
+	cameraGuiada1,
+	cameraGuiada2,
+	cameraGuiada3,
+	controls,
+	camOrbitControl,
+	camFirstPersonControl,
+	camFlyControl1,
+	camFlyControl2,
+	camFlyControl3;
+
+// Instancializando o objeto GUI
+var gui = new GUI();
+
+// Configurando as variáveis de luz para as luzes pontuais
+var lightColor = 0xffffff;
+var lightIntensity = 0.5;
+var lightDistance = 9.5;
+var posy = 3.0;
+
+// Configurando as posições das luzes pontuais, de acordo com os pontos de luz do cenário
+var camerasPos = [['i1', 'j1'], ['i4', 'j1'], ['i7', 'j1'], ['i10', 'j1'], ['i14', 'j1'],
+['i1', 'j2'], ['i15', 'j2'],
+['i3', 'j3'], ['i7', 'j3'], ['i11', 'j3'],
+['i5', 'j4'], ['i9', 'j4'], ['i13', 'j4'],
+['i14', 'j5'],
+['i6', 'j6'], ['i12', 'j6'],
+['i9', 'j7'], ['i15', 'j7'],
+['i1', 'j8'], ['i4', 'j8'],
+['i14', 'j9'],
+['i2', 'j10'], ['i5', 'j10'], ['i8', 'j10'], ['i12', 'j10'],
+['i3', 'j11'], ['i6', 'j11'], ['i10', 'j11'], ['i14', 'j11'],
+['i1', 'j12'], ['i5', 'j12'], ['i8', 'j12'], ['i12', 'j12'], ['i15', 'j12']];
+var coordI = {
+	'i1': -11.5, 'i2': -10.0, 'i3': -8.3, 'i4': -6.5, 'i5': -5.0, 'i6': -3.5, 'i7': -1.5,
+	'i8': 0.0, 'i9': 1.5, 'i10': 3.5, 'i11': 4.7, 'i12': 6.5, 'i13': 8.0, 'i14': 9.5, 'i15': 11.5
+};
+var coordJ = {
+	'j1': -10.5, 'j2': -7.5, 'j3': -5.5, 'j4': -4.0, 'j5': -2.5, 'j6': -1.0, 'j7': 0.5, 'j8': 2.0,
+	'j9': 3.5, 'j10': 5.0, 'j11': 8.0, 'j12': 11.5
+};
+
+const rendSize = new THREE.Vector2();
+const clock = new THREE.Clock();
+
+
+/// ***************************************************************
+/// ***                                                          **
+/// ***************************************************************
+
+function main() {
+
+	// Criando e instancializando o renderer no documento HTML
+	renderer = new THREE.WebGLRenderer();
+	renderer.setClearColor(new THREE.Color(0.0, 0.0, 0.0));
+	rendSize.x = window.innerWidth * 0.9999;
+	rendSize.y = window.innerHeight * 0.9999;
+	renderer.setSize(rendSize.x, rendSize.y);
+	document.body.appendChild(renderer.domElement);
+
+	// Criando e instancializando a cena e as câmeras
+	scene = new THREE.Scene();
+	cameraExterna = new THREE.PerspectiveCamera(45.0, window.innerWidth / window.innerHeight, 0.01, 500.0);
+	cameraInterna = new THREE.PerspectiveCamera(45.0, window.innerWidth / window.innerHeight, 0.01, 500.0);
+	cameraGuiada1 = new THREE.PerspectiveCamera(45.0, window.innerWidth / window.innerHeight, 0.01, 500.0);
+	cameraGuiada2 = new THREE.PerspectiveCamera(45.0, window.innerWidth / window.innerHeight, 0.01, 500.0);
+	cameraGuiada3 = new THREE.PerspectiveCamera(45.0, window.innerWidth / window.innerHeight, 0.01, 500.0);
+	cameraGuiada1.lookAt(30.0, 0.0, 5.0);
+	cameraGuiada2.lookAt(-30.0, 0.0, 5.0);
+	cameraGuiada3.lookAt(0.0, 0.0, 30.0);
+
+	// Adicionando luzes pontuais no modelo, simulando as lâmpadas do cenário
+	for (let i = 0; i < camerasPos.length; i++) {
+		console.log(camerasPos[i][0], coordI[camerasPos[i][0]]);
+		var pointLight = new THREE.PointLight(lightColor, lightIntensity, lightDistance);
+		pointLight.position.set(coordI[camerasPos[i][0]], posy, coordJ[camerasPos[i][1]]);
+		scene.add(pointLight);
+	}
+
+	// Carregando o modelo do Backrooms
+	const loaderGltf = new GLTFLoader();
+	loaderGltf.load('./assets/backrooms/scene.gltf', function (gltf) {
+		gltf.scene.scale.set(1.0, 1.0, 1.0);
+		gltf.scene.name = "backrooms";
+		scene.add(gltf.scene);
+		ajusteCamera();
+	})
+
+	// Carregando o primeiro modelo do Cientista
+	loaderGltf.load('./assets/doctors/scene.gltf', function (gltf) {
+		gltf.scene.scale.set(0.027, 0.027, 0.027);
+		gltf.scene.position.x = 8.0;
+		gltf.scene.position.y = 0.1;
+		gltf.scene.position.z = -3.3;
+		gltf.scene.name = "doctor";
+
+		scene.add(gltf.scene);
+		ajusteCamera();
+	});
+
+	// Carregando o segundo modelo do Cientista
+	loaderGltf.load('./assets/doctors/scene.gltf', function (gltf) {
+		gltf.scene.scale.set(0.027, 0.027, 0.027);
+		gltf.scene.position.x = -11.0;
+		gltf.scene.position.y = 0.1;
+		gltf.scene.position.z = 12.0;
+		gltf.scene.rotation.y = Math.PI / 1.5;
+		gltf.scene.name = "doctor";
+
+		scene.add(gltf.scene);
+		ajusteCamera();
+	});
+
+	// Carregando o modelo da Coisa
+	loaderGltf.load('./assets/thing/scene.gltf', function (gltf) {
+		gltf.scene.scale.set(0.5, 0.5, 0.5);
+		gltf.scene.name = "thing";
+		gltf.scene.position.x = -13.0;
+		gltf.scene.position.y = 0.1;
+		gltf.scene.position.z = -1.0;
+		gltf.scene.rotation.y = (Math.PI / 2.0) + 0.9;
+		scene.add(gltf.scene);
+		ajusteCamera();
+	});
+
+	// Configuranção do controle de câmera First Person, usada na visita interna
+	camFirstPersonControl = new FirstPersonControls(cameraInterna, renderer.domElement);
+	camFirstPersonControl.lookSpeed = 0.5;
+	camFirstPersonControl.movementSpeed = 10;
+	camFirstPersonControl.activeLook = false;
+	cameraInterna.position.x = 0.0;
+	cameraInterna.position.y = 1.8;
+	cameraInterna.position.z = 0.0;
+	camFirstPersonControl.target = new THREE.Vector3(0.0, 1.8, 0.0);
+
+	// Configuranção do controle de câmera Orbit, usada na visita externa
+	camOrbitControl = new OrbitControls(cameraExterna, renderer.domElement);
+	cameraExterna.position.x = 0.0;
+	cameraExterna.position.y = 25.0;
+	cameraExterna.position.z = 35.0;
+	camOrbitControl.target = new THREE.Vector3(0.0, 0.0, 0.0);
+
+	// Configuranção do controle de câmera Fly 1, usada na visita guiada
+	camFlyControl1 = new FlyControls(cameraGuiada1, renderer.domElement);
+	camFlyControl1.movementSpeed = 0.0;
+	camFlyControl1.rollSpeed = 0.0;
+	camFlyControl1.autoForward = false;
+	camFlyControl1.dragToLook = false;
+	cameraGuiada1.position.x = 0.0;
+	cameraGuiada1.position.y = 1.8;
+	cameraGuiada1.position.z = -4.8;
+
+	// Configuranção do controle de câmera Fly 2, usada na visita guiada
+	camFlyControl2 = new FlyControls(cameraGuiada2, renderer.domElement);
+	camFlyControl2.movementSpeed = 0.0;
+	camFlyControl2.rollSpeed = 0.0;
+	camFlyControl2.autoForward = false;
+	camFlyControl2.dragToLook = false;
+	cameraGuiada2.position.x = -12.0;
+	cameraGuiada2.position.y = 1.8;
+	cameraGuiada2.position.z = 11.3;
+	cameraGuiada2.rotation.y = Math.PI * -0.4;
+
+	// Configuranção do controle de câmera Fly 2, usada na visita guiada
+	camFlyControl3 = new FlyControls(cameraGuiada3, renderer.domElement);
+	camFlyControl3.movementSpeed = 0.0;
+	camFlyControl3.rollSpeed = 0.0;
+	camFlyControl3.autoForward = false;
+	camFlyControl3.dragToLook = false;
+	cameraGuiada3.position.x = -5.0;
+	cameraGuiada3.position.y = 1.8;
+	cameraGuiada3.position.z = -8.0;
+	cameraGuiada3.rotation.y = 1.0;
+
+	// Inicializando a GUI
+	initGUI();
+
+	// Chamando a função de renderização
+	render();
+};
+
+/// ***************************************************************
+/// ***                                                          **
+/// ***************************************************************
+function ajusteCamera() {
+
+	var obj = scene.getObjectByName("backrooms");
+
+	const helper = new THREE.BoxHelper();
+	helper.setFromObject(obj);
+	helper.geometry.computeBoundingBox();
+
+	const box = new THREE.Box3().setFromObject(obj);
+	var farPlane = Math.max(box.max.x, box.max.y, box.max.z);
+
+	cameraInterna.far = farPlane * 10.0;
+	cameraInterna.updateProjectionMatrix();
+
+	cameraExterna.far = farPlane * 10.0;
+	cameraExterna.updateProjectionMatrix();
+	camOrbitControl.autoRotate = controls.rotacaoAutomatica;
+}
+
+/// ***************************************************************
+/// ***                                                          **
+/// ***************************************************************
+function render() {
+
+	// Configurando os planos de corte
+	var xyPlane = new THREE.Plane(new THREE.Vector3(0, 0, 1), controls.alcanceCorte);
+	var xzPlane = new THREE.Plane(new THREE.Vector3(0, -1, 0), controls.alcanceCorte);
+	var yzPlane = new THREE.Plane(new THREE.Vector3(1, 0, 0), controls.alcanceCorte);
+	var globalPlane = new THREE.Plane(new THREE.Vector3(0, 0, 0), 1);
+
+	// Configurando o plano de corte de acordo com a seleção do usuário. Inicialmente, o plano de corte é o plano global
+	var plane = globalPlane;
+
+	// Configurando a câmera de acordo com a seleção do usuário
+	if (controls.tipoVisita == 'Externa') {
+
+		// Se o ponto de luz direcional Sun não estiver definida, define-a
+		if (scene.getObjectByName('sun') == null) {
+			var sun = new THREE.DirectionalLight(0xffffff, 0.5);
+			sun.castShadow = false;
+			sun.position.set(0, 5, 0);
+			sun.name = 'sun';
+			scene.add(sun);
+		}
+
+		// Se a luz ambiente Ambient não estiver definida, define-a
+		if (scene.getObjectByName('ambient') == null) {
+			var ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+			ambientLight.castShadow = true;
+			ambientLight.name = 'ambient';
+			scene.add(ambientLight);
+		}
+
+		// Configurando o plano de corte de acordo com a seleção do usuário
+		if (controls.cortePlano == 'Plano XY') {
+			plane = xyPlane;
+		} else if (controls.cortePlano == 'Plano XZ') {
+			plane = xzPlane;
+		} else if (controls.cortePlano == 'Plano YZ') {
+			plane = yzPlane;
+		} else {
+			plane = globalPlane;
+		}
+
+		camOrbitControl.update(clock.getDelta());
+		renderer.render(scene, cameraExterna);
+
+	} else if (controls.tipoVisita == 'Interna') {
+
+		// Removendo os pontos de luz direcional Sun e a luz ambiente Ambient
+		if (scene.getObjectByName('sun') != null) {
+			scene.remove(scene.getObjectByName('sun'));
+		}
+
+		if (scene.getObjectByName('ambient') != null) {
+			scene.remove(scene.getObjectByName('ambient'));
+		}
+
+		// Configurando o comportamento da câmera de acordo com a tecla pressionada
+		document.body.onkeyup = function (e) {
+			if (e.key == " " || e.code == "Spacebar") {
+				camFirstPersonControl.activeLook = !camFirstPersonControl.activeLook;
+			}
+		}
+
+		// Limitando o movimento da câmera para dentro do modelo
+		if (cameraInterna.position.x < -12.0) cameraInterna.position.x = -12.0;
+		if (cameraInterna.position.x > 12.0) cameraInterna.position.x = 12.0;
+		if (cameraInterna.position.z < -12.0) cameraInterna.position.z = -12.0;
+		if (cameraInterna.position.z > 12.0) cameraInterna.position.z = 12.0;
+
+		camFirstPersonControl.update(clock.getDelta());
+		cameraInterna.position.y = 1.8;
+		renderer.render(scene, cameraInterna);
+	} else {
+
+		// Removendo os pontos de luz direcional Sun e a luz ambiente Ambient
+		if (scene.getObjectByName('sun') != null) {
+			scene.remove(scene.getObjectByName('sun'));
+		}
+
+		if (scene.getObjectByName('ambient') != null) {
+			scene.remove(scene.getObjectByName('ambient'));
+		}
+
+		// Renderizando a cena de acordo com a câmera selecionada
+		if (controls.visitaGuiadaCamera == '1') {
+			camFlyControl1.update(clock.getDelta());
+			renderer.render(scene, cameraGuiada1);
+		} else if (controls.visitaGuiadaCamera == '2') {
+			camFlyControl2.update(clock.getDelta());
+			renderer.render(scene, cameraGuiada2);
+		} else {
+			camFlyControl3.update(clock.getDelta());
+			renderer.render(scene, cameraGuiada3);
+		}
+	}
+
+	// Cortando o modelo de acordo com o plano de corte selecionado
+	renderer.clippingPlanes = [plane];
+	renderer.localClippingEnabled = true;
+
+	requestAnimationFrame(render);
+}
+
+/// ***************************************************************
+/// ***                                                          **
+/// ***************************************************************
+
+
+function initGUI() {
+
+	// Configurando os controles da GUI
+	controls = {
+		tipoVisita: 'Interna',
+		visitaGuiadaCamera: '1',
+		cortePlano: 'Nenhum',
+		alcanceCorte: 0.1,
+		rotacaoAutomatica: true,
+	};
+
+	// Adicionando os controles da GUI
+	gui.add(controls, 'tipoVisita', ['Interna', 'Externa', 'Guiada']).onChange(ajusteCamera);
+	gui.add(controls, 'visitaGuiadaCamera', ['1', '2', '3']).onChange(ajusteCamera);
+	gui.add(controls, 'cortePlano', ['Nenhum', 'Plano XY', 'Plano XZ', 'Plano YZ']).onChange(ajusteCamera);
+	gui.add(controls, 'alcanceCorte', -17.0, 17.0).onChange(ajusteCamera);
+	gui.add(controls, 'rotacaoAutomatica').onChange(ajusteCamera);
+
+	// Abrindo a GUI
+	gui.open();
+};
+
+/// ***************************************************************
+/// ***************************************************************
+/// ***************************************************************
+
+main();
